d68cb425ea52eaecd82df882a8e8e49774ec2b26
Tomas <thomashjkl@protonmail.com>
2023-09-06 18:27:04 +0300

    classical "forgor moment"

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,13 +3,13 @@ export const routes = [
     { href: '/data_flow', name: 'Data flow', categoryId: 1 },
     { href: '/data_control', name: 'Data control', categoryId: 1 },
     { href: '/docs', name: 'Components', categoryId: 2 },
-    { href: '/docs/test', name: 'Testing', categoryId: 2 },
+    { href: '/docs/testing', name: 'Testing', categoryId: 2 },
     { href: '/resource/fpBasics', name: 'FP', categoryId: 3 },
 ];
 
 export const categories = [
     { id: 1, name: "Introduction" },
-    { id: 2, name: "Documents" },
+    { id: 2, name: "Documentation" },
     { id: 3, name: "Others" }
 ]
 
