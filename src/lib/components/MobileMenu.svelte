ff2310dfc70ff1580a3c9fb5550d80a840f67b6c
Tomas <thomashjkl@protonmail.com>
2024-03-27 23:13:36 +0200

    mobile menu

--- src/lib/components/MobileMenu.svelte
+++ src/lib/components/MobileMenu.svelte
@@ -4,6 +4,7 @@
 	import { page } from '$app/stores';
 	import { onMount, onDestroy } from 'svelte';
 	import { searchModal } from '$lib/stores/main';
+	import Links from './Links.svelte';
 
 	let menuZone: HTMLElement;
 
@@ -33,45 +34,33 @@
 <div transition:fly={{ x: -300, duration: 150, opacity: 1 }} class="menu" bind:this={menuZone}>
 	<nav>
 		<ul>
-			<!-- <li> -->
-			<!-- 	<button -->
-			<!-- 		class="searchButton" -->
-			<!-- 		on:click|preventDefault={() => { -->
-			<!-- 			searchModal.set(true); -->
-			<!-- 			closeSidebar(); -->
-			<!-- 		}}>Search</button -->
-			<!-- 	> -->
-			<!-- </li> -->
+			<!--
+      <li>
+				<button
+					class="searchButton"
+					on:click|preventDefault={() => {
+						searchModal.set(true);
+						closeSidebar();
+					}}>Search</button
+				>
+			</li>
+      -->
 			<li>
 				{#each categories as category}
 					<div class="title">{category.name}</div>
-					{#each routes.filter((item) => item.categoryId == category.id) as { href, name }}
+					{#each routes.filter((item) => item.categoryId == category.id) as { href, name, newTab }}
 						<li>
-							<a {href} on:click={closeSidebar} class:active={$page.url.pathname == href}>{name}</a>
+							<a
+								{href}
+								on:click={newTab ? () => {} : closeSidebar}
+								class:active={$page.url.pathname == href}
+								target={newTab ? '_blank' : ''}>{name}</a
+							>
 						</li>
 					{/each}
 				{/each}
-				<div class="other-links">
-					<li>
-						<a class="other" href="https://github.com/mimiMonads/vixeny" target="_blank">
-							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-								><path
-									fill="currentColor"
-									d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
-								/></svg
-							>
-						</a>
-					</li>
-					<li>
-						<a class="other" href="https://github.com/emastho/vixenydocs" target="_blank">
-							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-								><path
-									fill="currentColor"
-									d="m4.929 21.482l5.846-5.846a1.999 1.999 0 0 0 1.932-3.346a2 2 0 0 0-3.346 1.932l-5.846 5.846l-1.06-1.06c2.828-3.3 3.888-6.954 5.302-13.082l6.364-.707l5.657 5.657l-.707 6.363c-6.128 1.415-9.782 2.475-13.081 5.304l-1.061-1.06ZM16.596 2.037l6.347 6.346a.5.5 0 0 1-.277.848l-1.474.23l-5.656-5.657l.212-1.485a.5.5 0 0 1 .848-.282Z"
-								/></svg
-							>
-						</a>
-					</li>
+				<div>
+					<Links mobile={true} />
 				</div>
 			</li>
 		</ul>
@@ -79,10 +68,18 @@
 </div>
 
 <style>
+	ul {
+		margin: 0;
+	}
+
 	a {
-		font-size: 20px;
+		font-size: 18px;
 		display: block;
 		padding-block: 8px;
+	}
+
+	a:active {
+		text-decoration: underline;
 	}
 
 	.active {
@@ -93,6 +90,10 @@
 		font-size: 20px;
 		color: #464646;
 		padding-block: 16px;
+	}
+
+	.title:first-child {
+		padding-top: 0;
 	}
 
 	.menu {
@@ -107,16 +108,11 @@
 		overflow: scroll;
 	}
 
-	.other {
+	/* .other {
 		color: inherit;
-	}
+	} */
 
-	.other-links {
-		color: #464646;
-		padding-top: 16px;
-	}
-
-	.searchButton {
+	/* .searchButton {
 		font-size: 20px;
 		width: 100%;
 		border: none;
@@ -125,5 +121,5 @@
 		color: white;
 		border-radius: 3px;
 		padding-block: 4px;
-	}
+	} */
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,7 @@
 	import { accordion, searchModal } from '$lib/stores/main';
 	import nprogress from 'nprogress';
 	import { browser } from '$app/environment';
+	import Links from '$lib/components/Links.svelte';
 	let sidebarButton: HTMLElement;
 
 	beforeNavigate(() => {
@@ -88,37 +89,24 @@
 					<img src={Logo} alt="Logo" height="45" />
 				</a>
 			</div>
-			<!-- <button class="search" on:click={openSearch}> -->
-			<!-- 	<div> -->
-			<!-- 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" -->
-			<!-- 			><path -->
-			<!-- 				fill="currentColor" -->
-			<!-- 				d="m18.031 16.617l4.283 4.282l-1.415 1.415l-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9s9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617Zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.867-3.133-7-7-7s-7 3.133-7 7s3.133 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15Z" -->
-			<!-- 			/></svg -->
-			<!-- 		> -->
-			<!-- 		Search -->
-			<!-- 		<span>/</span> -->
-			<!-- 	</div> -->
-			<!-- </button> -->
+			<!--
+			<button class="search" on:click={openSearch}>
+				<div>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="m18.031 16.617l4.283 4.282l-1.415 1.415l-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9s9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617Zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.867-3.133-7-7-7s-7 3.133-7 7s3.133 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15Z"
+						/></svg
+					>
+					Search
+					<span>/</span>
+				</div>
+			</button>
+      -->
 			<div class="alternative" />
 			<Navigation />
 			<footer>
-				<a href="https://github.com/mimiMonads/vixeny" target="_blank">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-						><path
-							fill="currentColor"
-							d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
-						/></svg
-					>
-				</a>
-				<a href="https://github.com/emastho/vixenydocs" target="_blank">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-						><path
-							fill="currentColor"
-							d="m4.929 21.482l5.846-5.846a1.999 1.999 0 0 0 1.932-3.346a2 2 0 0 0-3.346 1.932l-5.846 5.846l-1.06-1.06c2.828-3.3 3.888-6.954 5.302-13.082l6.364-.707l5.657 5.657l-.707 6.363c-6.128 1.415-9.782 2.475-13.081 5.304l-1.061-1.06ZM16.596 2.037l6.347 6.346a.5.5 0 0 1-.277.848l-1.474.23l-5.656-5.657l.212-1.485a.5.5 0 0 1 .848-.282Z"
-						/></svg
-					>
-				</a>
+				<Links />
 			</footer>
 		</aside>
 		<section>
@@ -149,40 +137,40 @@
 		overflow-y: scroll;
 	}
 
-	/* .search { */
-	/* 	padding-block: 32px; */
-	/* 	font-size: 16px; */
-	/* 	color: #464646; */
-	/* 	display: flex; */
-	/* 	align-items: center; */
-	/* 	justify-content: center; */
-	/* 	cursor: pointer; */
-	/* 	user-select: none; */
-	/* 	display: flex; */
-	/* 	column-gap: 16px; */
-	/* 	background: none; */
-	/* 	outline: none; */
-	/* 	border: none; */
-	/* } */
+	/* .search {
+		padding-block: 32px;
+		font-size: 16px;
+		color: #464646;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		user-select: none;
+		display: flex;
+		column-gap: 16px;
+		background: none;
+		outline: none;
+		border: none;
+	} 
 
-	/* .search div { */
-	/* 	border: 2px solid #2b2b2b; */
-	/* 	padding: 8px; */
-	/* 	width: 100%; */
-	/* 	border-radius: 8px; */
-	/* 	display: flex; */
-	/* 	align-items: center; */
-	/* 	justify-content: space-around; */
-	/* } */
-	/**/
-	/* .search span { */
-	/* 	background: #2b2b2b; */
-	/* 	border-radius: 3px; */
-	/* 	padding-inline: 8px; */
-	/* 	padding-block: 2px; */
-	/* 	font-size: 12px; */
-	/* } */
+	.search div {
+		border: 2px solid #2b2b2b;
+		padding: 8px;
+		width: 100%;
+		border-radius: 8px;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
 
+	.search span {
+		background: #2b2b2b;
+		border-radius: 3px;
+		padding-inline: 8px;
+		padding-block: 2px;
+		font-size: 12px;
+	}
+*/
 	.logoArea {
 		display: flex;
 		justify-content: center;
@@ -192,11 +180,6 @@
 	footer {
 		margin-top: 64px;
 		text-align: center;
-		color: #464646;
-	}
-
-	footer a {
-		color: inherit;
 	}
 
 	section {
@@ -206,7 +189,7 @@
 		/* height: 100vh; */
 		padding-left: calc(270px + 4rem);
 		/* overflow-y: scroll; */
-		/*max-width: 120ch;*/
+		/* max-width: 120ch;*/
 		max-width: 85%;
 	}
 
